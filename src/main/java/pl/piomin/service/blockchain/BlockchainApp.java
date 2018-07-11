0acfde68ab8d8267d5480ca83f31ace8f0b85950
joaobini <Kdgnqmxc>
2018-07-11 15:05:45 -0300

    Resolving sonar issues and inserting swagger api

--- src/main/java/pl/piomin/service/blockchain/BlockchainApp.java
+++ src/main/java/pl/piomin/service/blockchain/BlockchainApp.java
@@ -2,18 +2,14 @@ package pl.piomin.service.blockchain;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.Transaction;
-import org.web3j.protocol.core.methods.response.EthAccounts;
 import org.web3j.protocol.core.methods.response.EthCoinbase;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
-import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import pl.piomin.service.blockchain.service.BlockchainService;
-import rx.Subscription;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -24,8 +20,11 @@ public class BlockchainApp {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BlockchainService.class);
 
-    @Autowired
-    Web3j web3j;
+    private final Web3j web3j;
+
+    public BlockchainApp(Web3j web3j) {
+        this.web3j = web3j;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BlockchainApp.class, args);
@@ -33,22 +32,33 @@ public class BlockchainApp {
 
     @PostConstruct
     public void listen() {
-        Subscription subscription = web3j.transactionObservable().subscribe(tx -> {
+
+        web3j.transactionObservable().subscribe(tx -> {
+
             LOGGER.info("New tx: id={}, block={}, from={}, to={}, value={}", tx.getHash(), tx.getBlockHash(), tx.getFrom(), tx.getTo(), tx.getValue().intValue());
+
             try {
+
                 EthCoinbase coinbase = web3j.ethCoinbase().send();
                 EthGetTransactionCount transactionCount = web3j.ethGetTransactionCount(tx.getFrom(), DefaultBlockParameterName.LATEST).send();
                 LOGGER.info("Tx count: {}", transactionCount.getTransactionCount().intValue());
+
                 if (transactionCount.getTransactionCount().intValue() % 10 == 0) {
+
                     EthGetTransactionCount tc = web3j.ethGetTransactionCount(coinbase.getAddress(), DefaultBlockParameterName.LATEST).send();
                     Transaction transaction = Transaction.createEtherTransaction(coinbase.getAddress(), tc.getTransactionCount(), tx.getValue(), BigInteger.valueOf(21_000), tx.getFrom(), tx.getValue());
                     web3j.ethSendTransaction(transaction).send();
+
                 }
+
             } catch (IOException e) {
                 LOGGER.error("Error getting transactions", e);
             }
+
         });
+
         LOGGER.info("Subscribed");
+
     }
 
 }
--- src/main/java/pl/piomin/service/blockchain/controller/BlockchainController.java
+++ src/main/java/pl/piomin/service/blockchain/controller/BlockchainController.java
@@ -1,27 +1,24 @@
 package pl.piomin.service.blockchain.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.web3j.crypto.CipherException;
 import pl.piomin.service.blockchain.model.BlockchainTransaction;
 import pl.piomin.service.blockchain.service.BlockchainService;
 
 import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 
 @RestController
 public class BlockchainController {
 
-    @Autowired
-    BlockchainService service;
+    private final BlockchainService service;
+
+    public BlockchainController(BlockchainService service) {
+        this.service = service;
+    }
 
     @PostMapping("/transaction")
-    public BlockchainTransaction execute(@RequestBody BlockchainTransaction transaction) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, CipherException, IOException {
+    public BlockchainTransaction execute(@RequestBody BlockchainTransaction transaction) throws IOException {
         return service.process(transaction);
     }
 
